e27fe0b18509c0134a8e20ee27c3a2afd6bcf18c
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-10-16 12:28:53 -0700

    added high-quality food and binoculars that work with the zoom bind

--- Half-Life-Apocalypse/ixhl2rp/schema/items/food/sh_ramen.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/food/sh_ramen.lua
@@ -4,9 +4,9 @@ ITEM.model = "models/props_junk/garbage_takeoutcarton001a.mdl"
 ITEM.category = "Food"
 ITEM.functions.Eat = {
 	icon = "icon16/cake.png",
-	sound = "player/footsteps/gravel1.wav",
+	sound = "npc/barnacle/barnacle_gulp2.wav",
 	OnRun = function(item)
-		item.player:addHunger(18000)
-		item.player:addThirst(5000)
+		item.player:addHunger(12000)
+		item.player:addThirst(2000)
 	end
 }
