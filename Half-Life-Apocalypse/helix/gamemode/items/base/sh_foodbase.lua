77983461811243ca8d8d185d2c6415db99891e13
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-10-17 22:11:50 -0700

    test

---
+++ Half-Life-Apocalypse/helix/gamemode/items/base/sh_foodbase.lua
@@ -0,0 +1,6 @@
+
+ITEM.name = "foodBase"
+ITEM.description = "Food Base"
+
+//literally made just to get rid of console error spam,
+//may be updated in the future for easier manipulation of food/drink items
