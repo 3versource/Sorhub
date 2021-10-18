040386e8ef3002e064407a18564673f988f75505
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-10-17 22:08:56 -0700

    patch to remove tests

--- Half-Life-Apocalypse/ixhl2rp/schema/items/food/sh_testingapple.lua
+++
@@ -1,8 +0,0 @@
-ITEM.base = "base_foodwater"
-ITEM.name = "Apple"
-ITEM.description = "A red Union-branded apple."
-ITEM.model = "models/bioshockinfinite/hext_apple.mdl"
-ITEM.category = "Food"
-ITEM.hungerRecov = 4000
-ITEM.thirstRecov = 10000
-ITEM.isFood = true
