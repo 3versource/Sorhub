a5e8a9bc14eb0ed8332b53e30170edf666a31ef6
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-10-24 19:09:35 -0700

    fiiiiix baaaaase errors

--- Half-Life-Apocalypse/ixhl2rp/schema/items/base/sh_medicalitems.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/base/sh_medicalitems.lua
@@ -2,7 +2,7 @@
 ITEM.name = "medBase"
 ITEM.description = "Medical base"
 ITEM.category = "Medical"
-ITEM.base = "base_medsitems"
+ITEM.base = "base_medsbase"
 
 //literally made just to get rid of console error spam,
 //may be updated in the future for easier manipulation of food/drink items
