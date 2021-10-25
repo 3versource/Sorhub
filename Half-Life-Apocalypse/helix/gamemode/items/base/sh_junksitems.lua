0eb4be9e6d91c7e9eadac36232f761f55474ea4f
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-10-24 19:05:44 -0700

    base fix

--- Half-Life-Apocalypse/helix/gamemode/items/base/sh_junksitems.lua
+++ Half-Life-Apocalypse/helix/gamemode/items/base/sh_junksitems.lua
@@ -1,7 +1,6 @@
 
 ITEM.name = "junkBase"
 ITEM.description = "Junk Base"
-ITEM.base = "base_junksbase"
 
 //literally made just to get rid of console error spam,
 //may be updated in the future for easier manipulation of food/drink items
--- Half-Life-Apocalypse/helix/gamemode/items/base/sh_medsitems.lua
+++ Half-Life-Apocalypse/helix/gamemode/items/base/sh_medsitems.lua
@@ -2,7 +2,6 @@
 ITEM.name = "medBase"
 ITEM.description = "Medical base"
 ITEM.category = "Medical"
-ITEM.base = "base_medsbase"
 
 //literally made just to get rid of console error spam,
 //may be updated in the future for easier manipulation of food/drink items
