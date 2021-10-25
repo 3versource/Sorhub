c435371822f6d72cb8567ecedee0df9709577b8e
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-10-24 17:47:49 -0700

    base fix

--- Half-Life-Apocalypse/helix/gamemode/items/base/sh_drink_base.lua
+++ Half-Life-Apocalypse/helix/gamemode/items/base/sh_drink_base.lua
@@ -2,6 +2,7 @@
 ITEM.name = "waterBase"
 ITEM.description = "Water Base"
 ITEM.category = "Drink"
+ITEM.base = "base_drink"
 
 //literally made just to get rid of console error spam,
 //may be updated in the future for easier manipulation of food/drink items
--- Half-Life-Apocalypse/helix/gamemode/items/base/sh_food_base.lua
+++ Half-Life-Apocalypse/helix/gamemode/items/base/sh_food_base.lua
@@ -2,6 +2,7 @@
 ITEM.name = "foodBase"
 ITEM.description = "Food Base"
 ITEM.category = "Food"
+ITEM.base = "base_food"
 
 //literally made just to get rid of console error spam,
 //may be updated in the future for easier manipulation of food/drink items
