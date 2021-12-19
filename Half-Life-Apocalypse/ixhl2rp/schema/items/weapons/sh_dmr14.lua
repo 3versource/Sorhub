1e02bd20c8390ef8dab6b64134f08f9ed74c5c59
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:46:51 -0800

    added a dmr14

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_dmr14.lua
@@ -0,0 +1,14 @@
+ITEM.name = "DMR-14"
+ITEM.description = "A DMR variant of the M14. It has a sleek appearance and utilizes 7.62x51mm NATO bullets."
+ITEM.model = "models/weapons/cw_dmr14/w_cw_dmr14.mdl"
+ITEM.class = "cw_dmr14"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 7,
+	pos	= Vector(-13, 360, -5)
+}
