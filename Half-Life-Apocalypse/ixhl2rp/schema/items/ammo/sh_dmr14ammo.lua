31f78351941a5ed680a0778d9102ab51dca363f3
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:49:13 -0800

    added 7.62x51 ammo

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/ammo/sh_dmr14ammo.lua
@@ -0,0 +1,6 @@
+ITEM.name = "7.62x51mm NATO Magazine"
+ITEM.model = "models/weapons/unloaded/snip_awp_mag.mdl"
+ITEM.ammo = "sniperpenetratedround"
+ITEM.ammoAmount = 20
+ITEM.description = "A magazine of 7.62x51mm NATO ammunition."
+ITEM.flag = "V"
