fda99c5848763788494cb94ee67aa79e6875458d
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:47:36 -0800

    added another revolver

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_357magnum.lua
@@ -0,0 +1,14 @@
+ITEM.name = ".357 Magnum"
+ITEM.description = "A double-action six-shot magnum chambered in .357. Unfortunately, it doesn't come with a speedloader, but can be fired at fast rates."
+ITEM.model = "models/weapons/cw_magnum/w_cw_magnum.mdl"
+ITEM.class = "cw_magnum"
+ITEM.weaponCategory = "sidearm"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 2
+ITEM.height = 1
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 2,
+	pos	= Vector(-6, 360, -5)
+}
