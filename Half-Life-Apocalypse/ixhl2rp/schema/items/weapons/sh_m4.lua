8874181752d85a949949350d5065f1c1684aaebd
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:46:39 -0800

    added an m4

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_m4.lua
@@ -0,0 +1,14 @@
+ITEM.name = "M4 Carbine"
+ITEM.description = "An american gas-powered rifle utilizing 5.56x45mm NATO."
+ITEM.model = "models/weapons/cw_xm4/w_cw_xm4.mdl"
+ITEM.class = "cw_xm4"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 6,
+	pos	= Vector(-9, 360, -5)
+}
