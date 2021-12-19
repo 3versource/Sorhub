b952e0fe2c07c41d3116402c27afe203ee5574cd
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:45:54 -0800

    added an auto shotgun

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_spas12auto.lua
@@ -0,0 +1,14 @@
+ITEM.name = "Spas-12 Semi-Auto"
+ITEM.description = "A devastating close-range semi-automatic."
+ITEM.model = "models/weapons/cw_gallo/w_cw_gallo.mdl"
+ITEM.class = "cw_gallo"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 4
+ITEM.height = 1
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 6,
+	pos	= Vector(-15, 360, -5)
+}
