9094cea4b43eaab83cddbeb0cbf9a588850a3088
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:48:13 -0800

    fixed the model for cheesecake

--- Half-Life-Apocalypse/ixhl2rp/schema/items/food/sh_cheesecake.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/food/sh_cheesecake.lua
@@ -1,6 +1,6 @@
 ITEM.name = "Cheesecake"
 ITEM.description = "A delicious slice of cheesecake. It's so velvety!"
-ITEM.model = "models/mosi/fallout4/props/food/radroachsteak.mdl"
+ITEM.model = "models/mosi/fallout4/props/food/preservedpie.mdl"
 ITEM.category = "Food"
 ITEM.functions.Eat = {
 	icon = "icon16/cake.png",
