77de0362c36081d8144e09fa380be6705ce44ec6
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:46:58 -0800

    added an ak5

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_ak5.lua
@@ -0,0 +1,14 @@
+ITEM.name = "AK-5"
+ITEM.description = "A Swedish assault rifle utilizing 5.56x45mm NATO."
+ITEM.model = "models/weapons/cw_krig6/w_cw_krig6.mdl"
+ITEM.class = "cw_krig6"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 6,
+	pos	= Vector(-9, 360, -5)
+}
