740b8ea4b5f3f9cc49dbe261a67d3d10af385a6c
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:46:45 -0800

    added an m16

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_m16.lua
@@ -0,0 +1,14 @@
+ITEM.name = "M16"
+ITEM.description = "A Vietnam War treasure, this is a 5.56x45mm NATO burst rifle."
+ITEM.model = "models/weapons/cw_m16/w_cw_m16.mdl"
+ITEM.class = "cw_m16"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 6,
+	pos	= Vector(-9, 360, -5)
+}
