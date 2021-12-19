93ecc43b7343c255e91c4af9fcb94c09eca321af
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:47:54 -0800

    added a 1911

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_1911.lua
@@ -0,0 +1,14 @@
+ITEM.name = "1911"
+ITEM.description = "A small compact pistol with high stopping power, once used by soldiers of the Great War. It utilizes .45 ACP bullets."
+ITEM.model = "models/weapons/cw_1911/w_cw_1911.mdl"
+ITEM.class = "cw_1911"
+ITEM.weaponCategory = "sidearm"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 2
+ITEM.height = 1
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 2,
+	pos	= Vector(-5, 360, -6)
+}
