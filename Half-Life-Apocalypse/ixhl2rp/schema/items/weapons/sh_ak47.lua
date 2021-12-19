44f65cf90765324baf4543605e7299b7e8ffb0db
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:47:04 -0800

    added an ak47

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_ak47.lua
@@ -0,0 +1,14 @@
+ITEM.name = "AK-47"
+ITEM.description = "A classic Russian full-auto gas-powered assault rifle that uses 7.62x39mm bullets."
+ITEM.model = "models/weapons/cw_ak47/w_cw_ak47.mdl"
+ITEM.class = "cw_ak47"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 6,
+	pos	= Vector(-9, 360, -5)
+}
