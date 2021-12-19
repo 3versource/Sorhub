ab06351838ac8c92a026a72076a262f6f494946c
OctraSource <90021750+OctraSource@users.noreply.github.com>
2021-12-19 00:46:35 -0800

    added mp5

---
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/weapons/sh_mp5.lua
@@ -0,0 +1,14 @@
+ITEM.name = "MP5"
+ITEM.description = "A compact gun used in close-range assaults. It utilizes 9x19 Parabellum bullets."
+ITEM.model = "models/weapons/cw_mp5/w_cw_mp5.mdl"
+ITEM.class = "cw_mp5"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EMP, CLASS_EOW}
+ITEM.flag = "V"
+ITEM.width = 3
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 5,
+	pos	= Vector(-3, 360, -5)
+}
