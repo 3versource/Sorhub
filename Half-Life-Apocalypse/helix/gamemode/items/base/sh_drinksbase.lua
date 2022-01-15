57fef244966441e8f3ead1bea3648559d0164d6e
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-01-14 16:13:01 -0800

    commented properly
    // is not the comment method in lua,
    -- is

--- Half-Life-Apocalypse/helix/gamemode/items/base/sh_drinksbase.lua
+++ Half-Life-Apocalypse/helix/gamemode/items/base/sh_drinksbase.lua
@@ -3,5 +3,5 @@ ITEM.name = "waterBase"
 ITEM.description = "Water Base"
 ITEM.category = "Drink"
 
-//literally made just to get rid of console error spam,
-//may be updated in the future for easier manipulation of food/drink items
+--literally made just to get rid of console error spam,
+--may be updated in the future for easier manipulation of food/drink items
