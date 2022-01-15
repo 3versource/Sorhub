301fb366a7701974efa1eb82a91bf561abcf0ad0
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-01-14 16:24:16 -0800

    food base function
    same explanation as the water base update

--- Half-Life-Apocalypse/helix/gamemode/items/base/sh_foodsbase.lua
+++ Half-Life-Apocalypse/helix/gamemode/items/base/sh_foodsbase.lua
@@ -2,6 +2,15 @@
 ITEM.name = "foodBase"
 ITEM.description = "Food Base"
 ITEM.category = "Food"
+ITEM.restorationT = 100
+ITEM.restorationH = 100
+ITEM.sound = "physics/flesh/flesh_squishy_impact_hard4.wav"
 
-//literally made just to get rid of console error spam,
-//may be updated in the future for easier manipulation of food/drink items
+--ITEM.functions.Eat = {
+--	icon = "icon16/cake.png",
+--	OnRun = function(item)
+--		item.player:addHunger(item.restorationT)
+--		item.player:addThirst(item.restorationH)
+--		item.player:EmitSound(item.sound)
+--	end
+--}
