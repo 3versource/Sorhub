b09b55f40d05d3908a6a7958f3589f66f620dcbf
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-03-31 12:35:26 -0700

    Update sh_metropolice.lua

--- Half-Life-Apocalypse/ixhl2rp/schema/factions/sh_metropolice.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/factions/sh_metropolice.lua
@@ -10,15 +10,26 @@ FACTION.runSounds = {[0] = "NPC_MetroPolice.RunFootstepLeft", [1] = "NPC_MetroPo
 
 function FACTION:OnCharacterCreated(client, character)
 	local inventory = character:GetInventory()
+	local id = Schema:ZeroNumber(math.random(1, 99999), 5)
 
 	inventory:Add("pistol", 1)
 	inventory:Add("stunstick", 1)
 	inventory:Add("pistolammo", 2)
 	inventory:Add("handheld_radio", 1)
+
+	-- new stuff, may not work
+	character:SetData("cid", id)
+
+	inventory:Add("cid", 1, {
+		name = character:GetName(),
+		id = id
+	})
+	-- end new stuff
 end
 
 function FACTION:GetDefaultName(client)
-	return "MPF-RCT." .. Schema:ZeroNumber(math.random(1, 99999), 5), true
+	return character:self:GetData("id", "00000")
+	--return "MPF-RCT." .. Schema:ZeroNumber(math.random(1, 99999), 5), true
 end
 
 function FACTION:OnTransfered(client)
@@ -32,18 +43,16 @@ function FACTION:OnNameChanged(client, oldValue, value)
 	local character = client:GetCharacter()
 
 	if (!Schema:IsCombineRank(oldValue, "RCT") and Schema:IsCombineRank(value, "RCT")) then
-		character:JoinClass(CLASS_MPR)
-	elseif (!Schema:IsCombineRank(oldValue, "OfC") and Schema:IsCombineRank(value, "OfC")) then
-	elseif (!Schema:IsCombineRank(oldValue, "EpU") and Schema:IsCombineRank(value, "EpU")) then
-		character:JoinClass(CLASS_EMP)
-	elseif (!Schema:IsCombineRank(oldValue, "DvL") and Schema:IsCombineRank(value, "DvL")) then
-	elseif (!Schema:IsCombineRank(oldValue, "SeC") and Schema:IsCombineRank(value, "SeC")) then
+		character:JoinClass(CLASS_MPR)s
+	end
+	else
+		-- experimental
+		character:JoinClass(CLASS_MPU)
+	end
+		
 	elseif (!Schema:IsCombineRank(oldValue, "SCN") and Schema:IsCombineRank(value, "SCN")
 	or !Schema:IsCombineRank(oldValue, "SHIELD") and Schema:IsCombineRank(value, "SHIELD")) then
 		character:JoinClass(CLASS_MPS)
-	end
-
-	if (!Schema:IsCombineRank(oldValue, "GHOST") and Schema:IsCombineRank(value, "GHOST")) then
 	end
 end
 
