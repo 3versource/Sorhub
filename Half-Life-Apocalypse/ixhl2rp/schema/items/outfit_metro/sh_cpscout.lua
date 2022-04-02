2f2be4c2f1a08894ebf20e6e17e08ca0f5609cf9
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-04-02 00:12:14 -0700

    category change

--- Half-Life-Apocalypse/ixhl2rp/schema/items/outfit_metro/sh_cpscout.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/outfit_metro/sh_cpscout.lua
@@ -5,8 +5,8 @@ ITEM.category = "Kevlar"
 ITEM.flag = "V"
 ITEM.maxArmor = 50
 ITEM.model = "models/props_junk/cardboard_box003b.mdl"
-ITEM.category = "Kevlar"
-ITEM.outfitCategory = "kevlar"
+ITEM.category = "Metropolice Uniform"
+ITEM.outfitCategory = "fullbody"
 
 ITEM.newSkin = 3
 ITEM.replacements = "models/police_nemez.mdl"
