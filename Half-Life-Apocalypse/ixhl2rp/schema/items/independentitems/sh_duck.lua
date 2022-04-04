ea02821faab5932c31bdd62a237ad85f92fd28fe
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-04-04 11:26:38 -0700

    category rename

--- Half-Life-Apocalypse/ixhl2rp/schema/items/independentitems/sh_duck.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/independentitems/sh_duck.lua
@@ -2,3 +2,5 @@
 ITEM.name = "Rubber Duck"
 ITEM.model = Model("models/sims/gm_ducky.mdl")
 ITEM.description = "A yellow rubber duck made for.. wait, what's it made for?"
+ITEM.category = "Junk"
+
--- Half-Life-Apocalypse/ixhl2rp/schema/items/independentitems/sh_padlock.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/independentitems/sh_padlock.lua
@@ -2,3 +2,5 @@
 ITEM.name = "Padlock"
 ITEM.model = Model("models/props_wasteland/prison_padlock001a.mdl")
 ITEM.description = "A padlock, used to lock storage."
+ITEM.category = "Junk"
+
