4ae6ff013b0989df0a34c194a7b8d2c59183430a
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-04-18 17:26:03 -0700

    correct ammo type

--- Half-Life-Apocalypse/ixhl2rp/schema/items/ammo/sh_mp5ammo.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/ammo/sh_mp5ammo.lua
@@ -1,6 +1,6 @@
 ITEM.name = "9x19mm Parabellum Magazine"
 ITEM.model = "models/weapons/unloaded/smg_mp5_mag.mdl"
-ITEM.ammo = "thumper"
+ITEM.ammo = "striderminigun"
 ITEM.ammoAmount = 30
 ITEM.description = "A magazine of 9x19mm Parabellum ammunition."
 ITEM.classes = {CLASS_EMP, CLASS_EOW}
