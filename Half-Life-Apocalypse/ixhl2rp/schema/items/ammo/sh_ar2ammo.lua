f2508f6e0093f3e014a292451b6d48ca2a5a337d
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-04-26 22:02:54 -0700

    ammo item changes

--- Half-Life-Apocalypse/ixhl2rp/schema/items/ammo/sh_ar2ammo.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/ammo/sh_ar2ammo.lua
@@ -1,5 +1,5 @@
 ITEM.name = "OSIPR Energy Magazine"
-ITEM.model = "models/items/combine_rifle_cartridge01_spent.mdl"
+ITEM.model = "models/Items/combine_rifle_cartridge01.mdl"
 ITEM.ammo = "AR2"
 ITEM.ammoAmount = 30
 ITEM.description = "A clamp magazine loaded into an other-worldly weapon."
--- Half-Life-Apocalypse/ixhl2rp/schema/items/ammo/sh_deagleammo.lua
+++ Half-Life-Apocalypse/ixhl2rp/schema/items/ammo/sh_deagleammo.lua
@@ -1,7 +1,7 @@
 ITEM.name = ".50 Magazine"
 ITEM.model = "models/weapons/unloaded/pist_deagle_mag.mdl"
 ITEM.ammo = "CombineHeavyCannon"
-ITEM.ammoAmount = 6
+ITEM.ammoAmount = 7
 ITEM.description = "A magazine of .50 ammunition."
 ITEM.classes = {CLASS_EOW}
 ITEM.flag = "V"
