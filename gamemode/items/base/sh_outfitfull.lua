674575ef23e0a91ff11acc05cf6919744a805b59
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-05-28 00:12:39 -0700

    Update sh_outfitfull.lua

--- gamemode/items/base/sh_outfitfull.lua
+++ gamemode/items/base/sh_outfitfull.lua
@@ -8,25 +8,6 @@ ITEM.outfitCategory = "model"
 ITEM.unitName = "unitName"
 ITEM.pacData = {}
 
---[[
--- This will change a player's skin after changing the model. Keep in mind it starts at 0.
-ITEM.newSkin = 1
--- This will change a certain part of the model.
-ITEM.replacements = {"group01", "group02"}
--- This will change the player's model completely.
-ITEM.replacements = "models/manhack.mdl"
--- This will have multiple replacements.
-ITEM.replacements = {
-	{"male", "female"},
-	{"group01", "group02"}
-}
--- This will apply body groups.
-ITEM.bodyGroups = {
-	["blade"] = 1,
-	["bladeblur"] = 1
-}
-]]--
-
 -- Inventory drawing
 if (CLIENT) then
 	function ITEM:PaintOver(item, w, h)
