0eea5b9044e0ec146960b1a2526430b462c71ff0
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-05-28 00:25:09 -0700

    full body armor (not cps)

---
+++ schema/items/outfit_fullbodyarmor/sh_eliterebeluniform.lua
@@ -0,0 +1,13 @@
+
+ITEM.name = "Kevlar Uniform"
+ITEM.description = "A uniform made of complete kevlar. A white lambda symbol is donned on the left armpad."
+ITEM.category = "Kevlar"
+ITEM.flag = "V"
+ITEM.maxArmor = 75
+ITEM.model = "models/props_junk/cardboard_box003b.mdl"
+ITEM.category = "Rebel Uniform"
+ITEM.outfitCategory = "fullbody"
+
+ITEM.replacements = {
+    {"models/ug_imports/lambdamovement/lambdamovement.mdl", "models/ug_imports/lambdamovement/lambdamovement_female.mdl"}
+    }
