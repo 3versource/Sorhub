e9addb06a5a79bb4ebba10a2300ea46d34742adc
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-10-27 18:01:34 -0700

    Functional bodygroup clothing (2)
    This needs to be made compatible with armor bodygroup clothes and model clothes.
    
    This setup currently has the capability of changing the player's bodygroups and skin, however complex it may be.

--- gamemode/items/base/sh_newoutfithandler.lua
+++ gamemode/items/base/sh_newoutfithandler.lua
@@ -4,19 +4,40 @@ ITEM.category = "Outfit"
 ITEM.model = "models/Gibs/HGIBS.mdl"
 ITEM.width = 1
 ITEM.height = 1
-ITEM.outfitCategory = "the clothing item's body selection"
--- currently: pants, torso, head, hands, eyes, mouth
+
+ITEM.outfitCategories = {}
+/*
+	citizen playermodel layout:
+	skin - 0
+	torso - 1
+	legs - 2
+	hands - 3
+	handgear - 4
+	bag - 5
+	glasses - 6
+	satchel - 7
+	pouch - 8
+	badge - 9
+	headstrap - 10
+	kevlar - 11
+	facialhair - 12
+
+	MPF playermodel layout:
+*/
+
 
 ITEM.playermodelBodygroupAndVariants = NULL -- table of pairs (bodygroup, variant), (bodygroup, variant)
 -- so if index 1 = 1 and index 2 = 1, bodygroup type 1 will be set to variant 1
 -- whatever the 1st bodygroup is of the model will have its variant changed to 1
-ITEM.playermodelBodygroupChanges = 1
--- the amount of bodygroup changes an item will have (default = 1)
+ITEM.playermodelBodygroupChanges = 0
+-- the amount of bodygroup changes an item will have (default = 0)
 
 -- ITEM.playermodel = NULL
 -- ITEM.playermodelSkin = NULL -- the new skin the player will have
 ITEM.isClothingItem = true
 ITEM.isBagItem = false
+ITEM.isMPFUniform = false
+ITEM.isArmor = false
 
 -- draws the small square on an item when the item is equipped
 if (CLIENT) then
@@ -28,7 +49,7 @@ if (CLIENT) then
 	end
 end
 
--- unequip the item
+-- the button to trigger the unequip process of the item
 ITEM.functions.EquipUn = { -- sorry, for name order.
 	name = "Unequip",
 	tip = "equipTip",
@@ -43,7 +64,7 @@ ITEM.functions.EquipUn = { -- sorry, for name order.
 	end
 }
 
--- equip the item
+-- the button to trigger the equip process of the item
 ITEM.functions.Equip = {
 	name = "Equip",
 	tip = "equipTip",
@@ -69,24 +90,36 @@ end
 function ITEM:OnEquipped(justChange, initialPly)
 	-- "self" refers to "item"
 	local ply = self.player or initialPly -- upon the hook being ran, self.player does not exist (because the item hasn't loaded yet)
-	local char = ply:GetCharacter()
-	local items = char:GetInv():GetItems() -- returns a table of the player's items to go through
 
-
+	-- changing without saving data about the player's model
 	if justChange then
-		for i = 1, (self.playermodelBodygroupChanges*2), 2 do
-			-- sets the player's model to the bodygroup specified
-			ply:SetBodygroup(self.playermodelBodygroupAndVariants[i], self.playermodelBodygroupAndVariants[i+1])
+		-- if there are bodygroup changes, then
+		if self.playermodelBodygroupAndVariants then
+			-- change the player's bodygroups
+			for i = 1, (self.playermodelBodygroupChanges*2), 2 do
+				-- sets the player's model to the bodygroup specified
+				ply:SetBodygroup(self.playermodelBodygroupAndVariants[i], self.playermodelBodygroupAndVariants[i+1])
+			end
 		end
 	else
+		-- defines this after the if statement to not waste time
+		local char = ply:GetCharacter() -- returns the player's character
+		local items = char:GetInv():GetItems() -- returns a table of the player's items to go through
+
+		-- go through the player's inventory
 		for k, v in pairs(items) do
-			if (v.id ~= self.id) then
+			-- if the selected item is a clothing item and its not the self item, then
+			if v.isClothingItem and v.id ~= self.id then
 				local itemTable = ix.item.instances[v.id]
 
-				-- if the selected item is a clothing item, is equipped, and is the same category as the super item, then
-				if v.isClothingItem and v:GetData("equip") and v.outfitCategory == self.outfitCategory then
-					-- unequip the conflicting item
-					v:OnUnequipped(self.player)
+				for i = 1, self.playermodelBodygroupChanges, 1 do
+					-- if the selected item is of the same category as the self item and is equipped, then
+					if v.outfitCategories[i] == self.outfitCategories[i] and v:GetData("equip") then
+						-- will unequip the selected item if ANY of the categories are the same to prevent conflictions
+
+						-- unequip the conflicting item
+						v:OnUnequipped(self.player)
+					end
 				end
 			end
 		end
@@ -136,7 +169,7 @@ end
 
 -- when the item is dropped, unequip it
 ITEM:Hook("drop", function(item)
-	if item:GetData("equip") then
+	if item:GetData("equip", false) then
 		item:OnUnequipped()
 	end
 end)
