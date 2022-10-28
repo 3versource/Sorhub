e94deb757157013133052b754f5ff3862c1cfe4c
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-10-28 12:34:16 -0700

    Update sh_outfit_dynamic.lua

--- schema/items/base/sh_outfit_dynamic.lua
+++ schema/items/base/sh_outfit_dynamic.lua
@@ -5,16 +5,36 @@ ITEM.category = "Outfit"
 ITEM.model = "models/Gibs/HGIBS.mdl"
 ITEM.width = 1
 ITEM.height = 1
-ITEM.outfitCategory = "the clothing item's body selection"
--- currently: pants, torso, head, hands, eyes, mouth
+ITEM.outfitCategories = {""}
+/*
+	citizen playermodel layout:
+	skin - 0
+	torso - 1
+	legs - 2
+	hands - 3
+	handgear - 4
+	bag - 5
+	glasses - 6
+	satchel - 7
+	pouch - 8
+	badge - 9
+	headstrap - 10
+	kevlar - 11
+	facialhair - 12
+
+	MPF playermodel layout:
+*/
+
 
 ITEM.playermodelBodygroupAndVariants = NULL -- table of pairs (bodygroup, variant), (bodygroup, variant)
 -- so if index 1 = 1 and index 2 = 1, bodygroup type 1 will be set to variant 1
 -- whatever the 1st bodygroup is of the model will have its variant changed to 1
-ITEM.playermodelBodygroupChanges = 1
--- the amount of bodygroup changes an item will have (default = 1)
+ITEM.playermodelBodygroupChanges = 0
+-- the amount of bodygroup changes an item will have (default = 0)
 
 -- ITEM.playermodel = NULL
 -- ITEM.playermodelSkin = NULL -- the new skin the player will have
 ITEM.isClothingItem = true
 ITEM.isBagItem = false
+ITEM.isMPFUniform = false
+ITEM.isArmor = false
